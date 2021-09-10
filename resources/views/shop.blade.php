088776545623d16f8c1c469259883cf3081ffd60
Ajith <ajithd78564@gmail.com>
2021-09-10 19:07:46 +0530

    some items removed

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -633,43 +633,6 @@
             </div>
           </section>
           <!-- !New Phones -->
-
-          <!-- Blogs -->
-            <section id="blogs">
-              <div class="container py-4">
-                <h4 class="font-rubik font-size-20">Latest Blogs</h4>
-                <hr>
-
-                <div class="owl-carousel owl-theme">
-                  <div class="item">
-                    <div class="card border-0 font-rale mr-5" style="width: 18rem;">
-                      <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
-                      <img src="../assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
-                      <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
-                      <a href="#" class="color-second text-left">Go somewhere</a>
-                    </div>
-                  </div>
-                  <div class="item">
-                    <div class="card border-0 font-rale mr-5" style="width: 18rem;">
-                      <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
-                      <img src="../assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
-                      <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
-                      <a href="#" class="color-second text-left">Go somewhere</a>
-                    </div>
-                  </div>
-                  <div class="item">
-                    <div class="card border-0 font-rale mr-5" style="width: 18rem;">
-                      <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
-                      <img src="../assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
-                      <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
-                      <a href="#" class="color-second text-left">Go somewhere</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-          <!-- !Blogs -->
-
         </main>
     <!-- !start #main-site -->
 
@@ -678,19 +641,7 @@
           <div class="container">
             <div class="row">
               <div class="col-lg-3 col-12">
-                <h4 class="font-rubik font-size-20">Mobile Shopee</h4>
-                <p class="font-size-14 font-rale text-white-50">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, deserunt.</p>
-              </div>
-              <div class="col-lg-4 col-12">
-                <h4 class="font-rubik font-size-20">Newslatter</h4>
-                <form class="form-row">
-                  <div class="col">
-                    <input type="text" class="form-control" placeholder="Email *">
-                  </div>
-                  <div class="col">
-                    <button type="submit" class="btn btn-primary mb-2">Subscribe</button>
-                  </div>
-                </form>
+                <h4 class="font-rubik font-size-20">ADshop</h4>
               </div>
               <div class="col-lg-2 col-12">
                 <h4 class="font-rubik font-size-20">Information</h4>
@@ -714,7 +665,7 @@
           </div>
         </footer>
         <div class="copyright text-center bg-dark text-white py-2">
-          <p class="font-rale font-size-14">&copy; Copyrights 2021. Desing By <a href="#" class="color-second">Ajith D</a></p>
+          <p class="font-rale font-size-14">&copy; Copyrights 2021. Desing By <a href="http://ajith.lovestoblog.com/" class="color-second">Ajith D</a></p>
         </div>
     <!-- !start #footer -->
 
